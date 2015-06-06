509fc9e6d32f0cf96d79aabd5af2d751689881c8
Michael Bartling <michael.bartling15+dev@gmail.com>
2015-06-06 12:47:39 -0500

    Added recent posts to homepage

--- routes/views/index.js
+++ routes/views/index.js
@@ -8,7 +8,32 @@ exports = module.exports = function(req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
-	
+		locals.data = {
+		posts: [],
+		categories: []
+	};
+		// Load the posts
+		view.on('init', function(next) {
+		
+		var q = keystone.list('Post').paginate({
+				page: req.query.page || 1,
+				perPage: 10,
+				maxPages: 1
+			})
+			.where('state', 'published')
+			.sort('-publishedDate')
+			.populate('author categories');
+		
+		if (locals.data.category) {
+			q.where('categories').in([locals.data.category]);
+		}
+		
+		q.exec(function(err, results) {
+			locals.data.posts = results;
+			next(err);
+		});
+		
+	});
 	// Render the view
 	view.render('index');
 	
