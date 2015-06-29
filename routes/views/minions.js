d3fc0179189df0bd98a73b6b2efe2ea002163e89
Michael Bartling <michael.bartling15+dev@gmail.com>
2015-06-29 12:19:19 -0500

    Added minions page models

--- routes/views/minions.js
+++ routes/views/minions.js
@@ -8,7 +8,33 @@ exports = module.exports = function(req, res) {
   // locals.section is used to set the currently selected
   // item in the header navigation.
   locals.section = 'minions';
-  
+  // locals.filters = {
+  //   user: req.params.user
+  // };
+  // locals.data = {
+  //   user: []
+  // };
+
+  // // Load the posts
+  // view.on('init', function(next) {
+    
+  //   var q = keystone.list('User').paginate({
+  //       page: req.query.page || 1,
+  //       perPage: 10,
+  //       maxPages: 10
+  //     })
+  //     // .where('isMinion', true)
+  //     .populate('name');
+    
+    
+  //   q.exec(function(err, results) {
+  //     locals.data.user = results;
+  //     next(err);
+  //   });
+    
+  // });
+  view.query('minions', keystone.list('User').model.find().sort('sortOrder'));
+
   // Render the view
   view.render('minions');
   
