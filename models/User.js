6edc97f3eaf153d3c9cf234ab28dc7db24478706
Michael Bartling <michael.bartling15+dev@gmail.com>
2015-06-29 11:18:58 -0500

    Added fields to user model. getting ready for finishing minion page

--- models/User.js
+++ models/User.js
@@ -11,7 +11,10 @@ var User = new keystone.List('User');
 User.add({
 	name: { type: Types.Name, required: true, index: true },
 	email: { type: Types.Email, initial: true, required: true, index: true },
-	password: { type: Types.Password, initial: true, required: true }
+	password: { type: Types.Password, initial: true, required: true },
+  bio: { type: Types.Html, wysiwyg: true, height: 150, required: false },
+  image: { type: Types.CloudinaryImage , required: false },
+  isMinion: {type: Boolean, label: 'is Minion', required: false },
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true }
 });
@@ -33,5 +36,5 @@ User.relationship({ ref: 'Post', path: 'posts', refPath: 'author' });
  * Registration
  */
 
-User.defaultColumns = 'name, email, isAdmin';
+User.defaultColumns = 'name, email, isAdmin, isMinion';
 User.register();
